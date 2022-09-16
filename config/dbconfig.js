5f1ea897ca32ef543d32bd00de5d9636f88ec7a5
aakash2809 <aakashrajak289@gmail.com>
2022-09-17 02:41:33 +0530

    refactor: move db url to .env

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,17 +1,14 @@
 
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/product-management'
 
 async function connectDb() {
-    try{
-        await mongoose.connect(url)
+    try {
+        await mongoose.connect(process.env.dbUrl);
         console.log('Successfully connected to the database');
-    }catch(error){
+    } catch(error) {
         console.log('Could not connect to the database. Exiting now...', error);
     }
 }
 
-module.exports = {
-    connectDb
-}
+module.exports = { connectDb }
 
