7edb138eb93eccdaa436bb69164bdf333502a0d3
aakash2809 <aakashrajak289@gmail.com>
2022-09-17 22:58:14 +0530

    create routes for product

--- app/route.js
+++ app/route.js
@@ -1,5 +1,6 @@
 const { register, login, } = require('./controllers/user');
 const { addCategory, updateCategoryByCategoryId, deleteCategryByCategryId } = require('./controllers/category');
+const { addProduct, getAllproduct, updateNoteByNoteId, deleteProduct } = require('./controllers/product');
 const { verifyToken } = require('./middleware/helper');
 
 class Routes {
@@ -16,6 +17,16 @@ class Routes {
       app.put('/update/category',verifyToken, updateCategoryByCategoryId);
       // Delete a category with categoryId
       app.delete('/delete/category',verifyToken, deleteCategryByCategryId);
+
+
+      // Create a new product
+      app.post('/add/product', verifyToken, addProduct);
+      // Retrieve all product
+      app.get('/all/product', verifyToken, getAllproduct);
+      //delete product
+      app.delete('/delete/product:productId', verifyToken, deleteProduct);
+     // Update a product detail with product Id
+      app.put('/update/product/:productId', verifyToken, updateNoteByNoteId);
     }
   }
   
